f4246c455af3b0b09e774109682a02be199318a0
iizvekov <iizvekov@aq.ru>
2024-01-16 16:11:30 +0300

    select component

--- src/components/select/select.jsx
+++ src/components/select/select.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./select.css"
 
-const Select = ({ options, onSelect, placeholder }) => {
-    const [selectedOption, setSelectedOption] = useState(placeholder);
+const Select = ({ options, onSelect }) => {
+    const [selectedOption, setSelectedOption] = useState(options.length !== 0 ? options[0].id : null);
+
+    useEffect(() => {
+        onSelect(selectedOption)
+    }, [])
 
     const handleSelectChange = (event) => {
         const selectedValue = event.target.value;
@@ -14,7 +18,7 @@ const Select = ({ options, onSelect, placeholder }) => {
         <select className="custom-select" value={selectedOption} onChange={handleSelectChange}>
             {options.map((option) => (
                 <option key={option.id} value={option.id}>
-                    {option.address}
+                    {option.name}
                 </option>
             ))}
         </select>
